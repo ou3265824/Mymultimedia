a8caa86d68cb27e965ed525af04076e0723f773b
ou3265824 <632521056@qq.com>
2017-02-16 23:43:39 +0800

    2017.2.16 环信

--- easeim/src/main/java/com/hyphenate/easeui/easeui/EaseConstant.java
+++ easeim/src/main/java/com/hyphenate/easeui/easeui/EaseConstant.java
@@ -14,6 +14,8 @@
 package com.hyphenate.easeui.easeui;
 
 public class EaseConstant {
+
+
     public static final String MESSAGE_ATTR_IS_VOICE_CALL = "is_voice_call";
     public static final String MESSAGE_ATTR_IS_VIDEO_CALL = "is_video_call";
     
